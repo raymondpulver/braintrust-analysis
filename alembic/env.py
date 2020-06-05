d166ed0ba342a1efa3975999db5c92faf090f50d
raymond <raymondpulver@my.uri.edu>
2020-06-05 21:47:02 +0000

    update

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,7 @@
-from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+from __future__ import with_statement
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+from logging.config import fileConfig
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +22,6 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
@@ -38,16 +35,10 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    context.configure(url=url)
 
     with context.begin_transaction():
         context.run_migrations()
-
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
@@ -56,22 +47,25 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    engine = engine_from_config(
+                config.get_section(config.config_ini_section),
+                prefix='sqlalchemy.',
+                poolclass=pool.NullPool)
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    connection = engine.connect()
+    context.configure(
+                connection=connection,
+                target_metadata=target_metadata
+                )
 
+    try:
         with context.begin_transaction():
             context.run_migrations()
-
+    finally:
+        connection.close()
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
---
+++ alembic/versions/4b81bd61b38_create_tables.py
@@ -0,0 +1,52 @@
+"""create tables
+
+Revision ID: 4b81bd61b38
+Revises: None
+Create Date: 2020-06-05 19:10:25.173140
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "4b81bd61b38"
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        "bars_1_min",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("ts", sa.Integer),
+        sa.Column("exchange", sa.CHAR(8)),
+        sa.Column("symbol", sa.CHAR(12)),
+        sa.Column("open", sa.FLOAT(8)),
+        sa.Column("high", sa.FLOAT(8)),
+        sa.Column("low", sa.FLOAT(8)),
+        sa.Column("close", sa.FLOAT(8)),
+        sa.Column("volume", sa.FLOAT(8)),
+        sa.Column("optional1", sa.FLOAT(8))
+    )
+   
+    op.create_table(
+        "perpfunding",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("ts", sa.Integer),
+        sa.Column("exchange", sa.CHAR(8), nullable=False),
+        sa.Column("symbol", sa.CHAR(12), nullable=False),
+        sa.Column("ts", sa.Integer, nullable=False),
+        sa.Column("value", sa.FLOAT(8), nullable=False),
+        sa.Column("optional1", sa.FLOAT(8))
+    )
+
+    pass
+
+
+def downgrade():
+
+    op.drop_table("bitmex",)
+    op.drop_table("binance",)
+    op.drop_table("binance_futures")
+    op.drop_table("perpfunding",)
+    pass
